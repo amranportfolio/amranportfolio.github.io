6e99a76a0f1022f798c8c62bce1d70a2612f429c
Amranahmed <39860421+Amranahmed@users.noreply.github.com>
2019-02-21 19:39:55 +0600

    yes

--- searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" method="get" id="searchform" class="searchform" action="http://localhost/learningwp/">
-	<div>
-		<label class="screen-reader-text" for="s">Search for:</label>
-		<input value="" name="s" id="s" type="text" placeholder="<?php the_search_query(); ?>" />
-		<input id="searchsubmit" value="Search" type="submit">
-	</div>
-</form>
